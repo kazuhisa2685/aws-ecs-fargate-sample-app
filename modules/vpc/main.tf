5919302fc74f4c4fa8a4c45fbb0a5269add3236a
Kazuhisa Nakajima <kazuhisa2685@gmail.com>
2026-08-07 20:38:56 +0900

    SG完成。あとからチューニング予定

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -351,3 +351,31 @@ resource "aws_security_group" "aurora_sg" {
     Env     = var.environment
   }
 }
+
+# VPCエンドポイント（インターフェイス型だからENIがつくられるため）のセキュリティグループ
+resource "aws_security_group" "vpc_endpoint_sg" {
+  name        = "${var.project}-${var.environment}-vpc-endpoint-sg"
+  description = "Security group for VPC Endpoints"
+  vpc_id      = aws_vpc.vpc.id
+
+  # アプリケーションサブネット内のリソースからのリクエストのみ許可する
+  ingress {
+    from_port       = 443
+    to_port         = 443
+    protocol        = "tcp"
+    security_groups = [aws_security_group.fargate_backend_sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name    = "${var.project}-${var.environment}-vpc-endpoint-sg"
+    Project = var.project
+    Env     = var.environment
+  }
+}
