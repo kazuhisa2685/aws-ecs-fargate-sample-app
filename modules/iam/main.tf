9c1a4036c7f12cb6d985b54523dae89fad3826f0
Kazuhisa Nakajima <kazuhisa2685@gmail.com>
2026-09-06 20:48:21 +0900

    #3バックエンドECS　⇒　Auroraへの接続ができない件について-追加修正2

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -90,3 +90,23 @@ resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
   role       = aws_iam_role.ecs_task_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
+
+# ECSタスク実行用のIAMロールにSecrets Managerへのアクセス権限を付与
+
+resource "aws_iam_role_policy" "get_secret_values" {
+  name = "GetSercretValues"
+  role = aws_iam_role.ecs_task_execution_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = [
+          "secretsmanager:GetSecretValue"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
