f49b96959f50604f1269d80961a1b67ec51c6eb3
Kazuhisa Nakajima <kazuhisa2685@gmail.com>
2026-09-07 19:58:57 +0900

    テストPUSH

--- docker/sample-dev-backend/app/database.py
+++ docker/sample-dev-backend/app/database.py
@@ -9,6 +9,7 @@ DB_HOST = os.getenv("DB_HOST", "localhost")
 DB_PORT = os.getenv("DB_PORT", "5432")
 DB_NAME = os.getenv("DB_NAME", "sampledb")
 
+# SQLの接続URLを作成
 DATABASE_URL = (
     f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 )
