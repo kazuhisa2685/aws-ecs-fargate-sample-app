35a2c866bd8824b42eb21ec05417dbff4d6dfe18
Kazuhisa Nakajima <kazuhisa2685@gmail.com>
2026-09-09 07:23:00 +0900

    戻し

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -172,7 +172,7 @@ resource "aws_lb_target_group" "backend_target_1" {
   vpc_id      = var.vpc_id
 
   health_check {
-    path                = "/health"
+    path                = "/"
     interval            = 60
     timeout             = 5
     healthy_threshold   = 3
@@ -196,7 +196,7 @@ resource "aws_lb_target_group" "backend_target_2" {
   vpc_id      = var.vpc_id
 
   health_check {
-    path                = "/health"
+    path                = "/"
     interval            = 60
     timeout             = 5
     healthy_threshold   = 3
