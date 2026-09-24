8a4f1383ffd9a086e9d16d6f28f222cf26233e18
Kazuhisa Nakajima <kazuhisa2685@gmail.com>
2026-09-24 18:14:59 +0900

    修正

--- modules/cloudwatch/main.tf
+++ modules/cloudwatch/main.tf
@@ -28,8 +28,8 @@ resource "aws_cloudwatch_metric_alarm" "frontend_metric_alarm" {
   alarm_name          = "${var.project}-${var.environment}-frontend-2xx"
   comparison_operator = "GreaterThanOrEqualToThreshold"  # 閾値以上になったら
   evaluation_periods  = 1                                # 判定を行う評価期間の数（例: 1回）
-  metric_name         = aws_cloudwatch_metric_filter.frontend_metric_filter.metric_transformation[0].name
-  namespace           = aws_cloudwatch_metric_filter.frontend_metric_filter.metric_transformation[0].namespace
+  metric_name         = aws_cloudwatch_log_metric_filter.frontend_metric_filter.metric_transformation[0].name
+  namespace           = aws_cloudwatch_log_metric_filter.frontend_metric_filter.metric_transformation[0].namespace
   period              = 60                               # 集計期間（秒）: 1分間
   statistic           = "Sum"                            # 集計方法: 合計値
   threshold           = 1                                # 閾値: 1回以上発生したら
@@ -44,8 +44,8 @@ resource "aws_cloudwatch_metric_alarm" "backend_metric_alarm" {
   alarm_name          = "${var.project}-${var.environment}-backend-2xx"
   comparison_operator = "GreaterThanOrEqualToThreshold"  # 閾値以上になったら
   evaluation_periods  = 1                                # 判定を行う評価期間の数（例: 1回）
-  metric_name         = aws_cloudwatch_metric_filter.backend_metric_filter.metric_transformation[0].name
-  namespace           = aws_cloudwatch_metric_filter.backend_metric_filter.metric_transformation[0].namespace
+  metric_name         = aws_cloudwatch_log_metric_filter.backend_metric_filter.metric_transformation[0].name
+  namespace           = aws_cloudwatch_log_metric_filter.backend_metric_filter.metric_transformation[0].namespace
   period              = 60                               # 集計期間（秒）: 1分間
   statistic           = "Sum"                            # 集計方法: 合計値
   threshold           = 1                                # 閾値: 1回以上発生したら
