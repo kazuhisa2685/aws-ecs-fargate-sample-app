71dae704acc5712a344d4a9176cd3afdf57ca6cf
Kazuhisa Nakajima <kazuhisa2685@gmail.com>
2026-09-24 18:13:10 +0900

    修正

--- modules/cloudwatch/main.tf
+++ modules/cloudwatch/main.tf
@@ -1,4 +1,4 @@
-resource "aws_cloudwatch_metric_filter" "frontend_metric_filter" {
+resource "aws_cloudwatch_log_metric_filter" "frontend_metric_filter" {
   name           = "${var.project}-${var.environment}-frontend-metric_filter"
   log_group_name = var.ecs_frontend_log_group_name
   pattern        = "{ $.statusCode = 200 }"
@@ -11,7 +11,7 @@ resource "aws_cloudwatch_metric_filter" "frontend_metric_filter" {
   }
 }
 
-resource "aws_cloudwatch_metric_filter" "backend_metric_filter" {
+resource "aws_cloudwatch_log_metric_filter" "backend_metric_filter" {
   name           = "${var.project}-${var.environment}-frontend-metric_filter"
   log_group_name = var.ecs_backend_log_group_name
   pattern        = "{ $.statusCode = 200 }"
@@ -24,7 +24,7 @@ resource "aws_cloudwatch_metric_filter" "backend_metric_filter" {
   }
 }
 
-resource "aws_cloudwatch_log_metric_alarm" "frontend_metric_alarm" {
+resource "aws_cloudwatch_metric_alarm" "frontend_metric_alarm" {
   alarm_name          = "${var.project}-${var.environment}-frontend-2xx"
   comparison_operator = "GreaterThanOrEqualToThreshold"  # 閾値以上になったら
   evaluation_periods  = 1                                # 判定を行う評価期間の数（例: 1回）
@@ -40,7 +40,7 @@ resource "aws_cloudwatch_log_metric_alarm" "frontend_metric_alarm" {
   # alarm_actions     = [aws_sns_topic.log_alarm.arn]
 }
 
-resource "aws_cloudwatch_log_metric_alarm" "backend_metric_alarm" {
+resource "aws_cloudwatch_metric_alarm" "backend_metric_alarm" {
   alarm_name          = "${var.project}-${var.environment}-backend-2xx"
   comparison_operator = "GreaterThanOrEqualToThreshold"  # 閾値以上になったら
   evaluation_periods  = 1                                # 判定を行う評価期間の数（例: 1回）
