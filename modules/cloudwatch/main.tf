502c34003a4ee5e82475483a7eda2a8a3f6f4e43
Kazuhisa Nakajima <kazuhisa2685@gmail.com>
2026-09-24 18:43:28 +0900

    修正

--- modules/cloudwatch/main.tf
+++ modules/cloudwatch/main.tf
@@ -12,7 +12,7 @@ resource "aws_cloudwatch_log_metric_filter" "frontend_metric_filter" {
 }
 
 resource "aws_cloudwatch_log_metric_filter" "backend_metric_filter" {
-  name           = "${var.project}-${var.environment}-frontend-metric_filter"
+  name           = "${var.project}-${var.environment}-backend-metric_filter"
   log_group_name = var.ecs_backend_log_group_name
   pattern        = "{ $.statusCode = 200 }"
 
